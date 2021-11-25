418d7c0bc19056e9549152701196588b8b5016fc
Murilo S. Amaral <murimaral@gmail.com>
2021-11-25 17:45:15 -0300

    added resizing function width and heigth

--- image_module/image_changer.py
+++ image_module/image_changer.py
@@ -5,13 +5,22 @@ import os
 
 main_path = os.getcwd()
 def option():
-    print('Choose one option\n1:rotate\n2:resize\n0:cancel and exit')
+    ### Displays menu and triggers one of the following functions
+    print('Choose one option\n1:rotate\n2:resize\n<anything-else>:cancel and exit')
     choose = str(input('numbers: '))
     if choose == '1':
         x = int(input('Please insert a angle to rotate anti-clockwise\n'))
         print('Starting to rotate by ', x, ' degrees')
         new_rotat(x)
+    elif choose == '2':
+        heigth = int(input('Choose new heigth:\n'))
+        width = int(input('Choose new width:\n'))
+        new_size(width, heigth)
+    else:
+        print('Canceling and exiting. Good-bye! :)\n')
+
 def new_rotat(a):
+    ### Rotates anti-clockwise
     images = os.listdir(main_path+'/images')
     image_dir = main_path + '/images/'
     new_image_dir = main_path + '/changed_images/'
@@ -21,7 +30,17 @@ def new_rotat(a):
             print('Successfully rotation of ', a, ' degrees')
         except:
             print('Could not convet this file')
-
+def new_size(a,b):
+    ### Resizes: a = width and b = heigth
+    images = os.listdir(main_path+'/images')
+    image_dir = main_path + '/images/'
+    new_image_dir = main_path + '/changed_images/'
+    for image in images:
+        try:
+            Image.open(image_dir+ image).resize((a,b)).convert('RGB').save(new_image_dir+image)                         
+            print('Successfully resizing: ', a, ' width and ', b, ' heigth')                                                   
+        except: 
+            print('Could not convet this file')
 
 def main():
   option()
