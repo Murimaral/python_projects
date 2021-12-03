1b7cdb7ef288b5eb080f490bfaf9283ca245b3b5
Murilo S. Amaral <murimaral@gmail.com>
2021-12-03 13:28:10 -0300

    WIP search by cases

--- covid_19_API/covid_API_v1.py
+++ covid_19_API/covid_API_v1.py
@@ -2,15 +2,45 @@
 
 import requests
 
+api_base = "https://covid-api.mmediagroup.fr/v1"
+
 def menu():
   print("Welcome to covid-19 search:\nPlease choose one of the following options:\n")
-  choice = input("1: Search Cases\n2: History\n3: Vaccines\n<Anyother> Cancel and quit\n")
+  choice = input("1: Search Cases\n2: History\n3: Vaccines\n<Anything else> Cancel and quit\n")
   switch = { "1": option_cases }
   case =  switch.get(choice, default)
   case()
 
 def option_cases():
-  print("Option Cases :)\n")
+  choice = input("To continue, please, choose a search method:\n1: COUNTRY NAME(i.e. France)\n2: COUNTRY ABREVIATION(i.e. FR)\n3: CONTINENT(i.e. Europe)\n<Anything else>: Drop and quit\n")
+  endpoint = "/cases"
+  info = ""
+  switch = {"1": input, "2": input, "3": input}
+  case = switch.get(choice, default)
+  if choice == "1":
+    info = case("Input a country name:\n")
+    response = requests.get(api_base+endpoint, params={ "country": info })
+    if not response.ok:
+      print("Failed requests, status code: {}".format(response.status_code))
+      option_cases()
+    print(response.json())
+  elif choice =="2":
+    info = case("Input a country abreviation:\n")
+    response = requests.get(api_base+endpoint, params={ "ab": info })
+    if not response.ok:
+      print("Failed requests, status code: {}".format(response.status_code))
+      option_cases()
+    print(response.json())
+  elif choice =="3":
+    info = case("Input a continent name:\n")
+    response = requests.get(api_base+endpoint, params={ "continent": info })
+    if not response.ok:
+      print("Failed requests, status code: {}".format(response.status_code))
+      option_cases()
+    print(response.json())
+  else:
+    case()
+  
 def default():
   print("Canceling and quitting.\nGood-bye :)\n")
   
